a4f9741277d48c0886776ef5f9de4f75cc8352c7
Julia <julialn.sunni@gmail.com>
2020-12-28 10:40:23 -0500

    create password reset request form

--- app/forms.py
+++ app/forms.py
@@ -51,3 +51,7 @@ class PostForm(FlaskForm):
 
 class EmptyForm(FlaskForm):
   submit = SubmitField('Submit')
+
+class ResetPasswordRequestForm(FlaskForm):
+  email = StringField('Email', validators=[DataRequired(), Email()])
+  submit = SubmitField('Request Password Reset')
--- app/routes.py
+++ app/routes.py
@@ -3,8 +3,9 @@ from werkzeug.urls import url_parse
 from datetime import datetime
 from flask_login import current_user, login_user, logout_user, login_required
 from app import app, db
-from app.forms import LoginForm, RegistrationForm, EditProfileForm, PostForm, EmptyForm
+from app.forms import LoginForm, RegistrationForm, EditProfileForm, PostForm, EmptyForm, ResetPasswordRequestForm
 from app.models import User, Post
+from app.email import send_password_reset_email
 
 @app.before_request
 def before_request():
@@ -152,3 +153,17 @@ def unfollow(username):
     return redirect(url_for('user', username=username))
   else:
     return redirect(url_for('index'))
+
+@app.route('/reset_password_request', methods=['GET', 'POST'])
+def reset_password_request():
+  if current_user.is_authenticated:
+    return redirect(url_for('index'))
+  form = ResetPasswordRequestForm()
+  if form.validate_on_submit():
+    user = User.query.filter_by(email=form.email.data).first()
+    if user:
+      send_password_reset_email(user)
+    flash('Check your email for the instructions to reset your password.')
+    return redirect(url_for('login'))
+  return render_template('reset_password_request.html', 
+    title='Reset Password', form=form)
